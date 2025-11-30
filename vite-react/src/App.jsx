70fcfb39197cfe3c264a8b1469ceffc46663b925
mominahmadrao <186613410+mominahmadrao@users.noreply.github.com>
2025-11-30 09:44:54 +0500

    Understanding approach of JS and components in React and how to use them

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,8 +1,14 @@
+import Youtube from './Youtube';
+
+const username = "Momin";
 function App() {
-  return(
-  <div class="text-3xl font-bold underline bg-amber-900">
-    Chai Aur react with vite | Momin
-  </div>
+  return (
+    <div>
+      Chai Aur react with vite | Momin
+      <h1> Vite react app{2+2}</h1>
+      <h1>Vite React app{username} </h1>
+      <Youtube />
+    </div>
   );
 }
 
--- vite-react/src/main.jsx
+++ vite-react/src/main.jsx
@@ -1,10 +1,42 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+
+const reactElement = {
+  type: 'a',
+  props: {
+    href: 'https://google.com',
+    target: '_blank',
+  }, // attributes
+  childern: 'Click me to visit people',
+};
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom React app</h1>
+    </div>
+  );
+}
+
+const areactElement = React.createElement(
+  'a',
+  { href: 'https://google.com', target: '_blank' },
+  'click to visit google'
+);
+const AnotherElement = (
+  <a href="http://google.com" target="_blank">
+    Google
+  </a>
+);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <>
+  {areactElement}
+  <App />
+   {/* reactElement
+   AnotherElement
+   <MyApp /> */}
+  </>
 )
