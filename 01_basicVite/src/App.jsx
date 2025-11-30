fb7790053154aba71132e5864400d7debd20587c
mominahmadrao <186613410+mominahmadrao@users.noreply.github.com>
2025-11-30 18:45:08 +0500

    Adjusting file names

---
+++ 01_basicVite/src/App.jsx
@@ -0,0 +1,15 @@
+import Youtube from './Youtube';
+
+const username = "Momin";
+function App() {
+  return (
+    <div>
+      Chai Aur react with vite | Momin
+      <h1> Vite react app{2+2}</h1>
+      <h1>Vite React app{username} </h1>
+      <Youtube />
+    </div>
+  );
+}
+
+export default App;
---
+++ 03_tailwindProps/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import Card from './components/Card';
+
+function App() {
+  const [count, setCount] = useState(0);
+  let myObj = {
+    username: 'hitesh',
+    age: 21,
+  };
+  let newArr = [1, 2, 3];
+
+  return (
+    <>
+      <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">
+        Tailwind test
+      </h1>
+      <Card username="chaiaurcode" btnText="click me" />
+      <Card username="hitesh" myArray={[1, 2, 3, 4]} />
+      <Card />
+    </>
+  );
+}
+export default App;
