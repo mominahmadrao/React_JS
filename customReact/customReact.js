67ff65581d4a4260e71079dc76e85a5768795653
mominahmadrao <186613410+mominahmadrao@users.noreply.github.com>
2025-11-30 09:42:48 +0500

    Understanding how react works

---
+++ customReact/customReact.js
@@ -0,0 +1,27 @@
+function customRender(reactElement, container) {
+  /*const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.childern;
+  domElement.setAttribute('href', reactElement.props.href);
+  domElement.setAttribute('target', reactElement.props.target);
+  container.appendChild(domElement); */
+
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.childern;
+  for (const prop in reactElement.props) {
+    domElement.setAttribute(prop, reactElement.props[prop]);
+  }
+  container.appendChild(domElement);
+}
+
+const reactElement = {
+  type: 'a',
+  props: {
+    href: 'https://google.com',
+    target: '_blank',
+  }, // attributes
+  childern: 'Click me to visit people',
+};
+
+const mainContainer = document.querySelector('#root');
+
+customRender(reactElement, mainContainer);
