eabd97e624c19b22b79e8ffdc3d0359c8f268763
mominahmadrao <186613410+mominahmadrao@users.noreply.github.com>
2026-03-07 07:29:54 +0500

    Securely Add github api token

--- 07_reactRouter/src/components/Github/Github.jsx
+++ 07_reactRouter/src/components/Github/Github.jsx
@@ -3,36 +3,41 @@ import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 function Github() {
+  // Better approach for pre processing
+  const data = useLoaderData();
+  return (
+    <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
+      <h1>{data.login}</h1>
+      <img src={data.avatar_url} alt="github avatar" width={200} />
+    </div>
+  );
 
-    // Better approach for pre processing
-    const data = useLoaderData()
+  //    const [data,setData] = useState([]);
 
-//    const [data,setData] = useState([]);
-   
-//    useEffect(( ) =>{
-//      fetch('https://api.github.com/users/mominahmadrao')
-//      .then((response) => response.json() )
-//      .then(data => {
-//         console.log(data)
-//         setData(data)
-//      })
-//    } , [])
+  //    useEffect(( ) =>{
+  //      fetch('https://api.github.com/users/mominahmadrao')
+  //      .then((response) => response.json() )
+  //      .then(data => {
+  //         console.log(data)
+  //         setData(data)
+  //      })
+  //    } , [])
 
-//     return (
-//     <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
-//       Github username: {data.login};
-//       <img src={data.avatar_url} width={300} alt="" />
-//     </div>
-//   );
-
-
+  //     return (
+  //     <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
+  //       Github username: {data.login};
+  //       <img src={data.avatar_url} width={300} alt="" />
+  //     </div>
+  //   );
 }
-
 
 export default Github;
 
-
 export const githubInfoLoader = async () => {
-    const response = await fetch('https://api.github.com/users/mominahmadrao')
-    return response.json()
-}
+  const response = await fetch("https://api.github.com/users/mominahmadrao", {
+    headers: {
+      Authorization: `token ${import.meta.env.VITE_GITHUB_TOKEN}`,
+    },
+  });
+  return response.json();
+};
