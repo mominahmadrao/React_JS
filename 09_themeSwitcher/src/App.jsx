77f174c59c5f193219faf5329800ac0dc5b234e2
mominahmadrao <186613410+mominahmadrao@users.noreply.github.com>
2026-05-16 11:58:27 +0500

    Completed theme switcher using context api

--- 09_themeSwitcher/src/App.jsx
+++ 09_themeSwitcher/src/App.jsx
@@ -1,7 +1,7 @@
 
 import { useEffect, useState } from 'react'
 import { ThemeProvider } from './context/theme.js'
-import ThemeBtn from './components/ThemeButton.jsx'
+import ThemeButton from './components/ThemeButton.jsx'
 import Card from './components/Card.jsx'
 
 function App() {
@@ -28,7 +28,7 @@ function App() {
       <div className="flex flex-wrap min-h-screen items-center">
           <div className="w-full">
               <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                  <ThemeBtn />
+                  <ThemeButton />
               </div>
 
               <div className="w-full max-w-sm mx-auto">
